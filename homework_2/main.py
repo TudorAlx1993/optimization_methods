4406c3b3f0ffe90d669baa6fea83cc59f2dcc5df
Tudor-Alexandru ALEXANDRESCU <tudor.alexandrescu@s.unibuc.ro>
2024-04-05 21:15:55 +0300

    Code update.

--- homework_2/main.py
+++ homework_2/main.py
@@ -4,6 +4,7 @@ import pickle
 import cvxpy as cp
 import numpy as np
 import matplotlib.pyplot as plt
+import timeit
 
 
 def main():
@@ -26,7 +27,7 @@ def main():
 
     # punctul b)
     D_matrix = generate_D_matrix(n)
-    lambda_values = [1, 5, 10, 15, 20,100]
+    lambda_values = [1, 5, 10, 15, 20, 100]
     x_star_cvxpy = {}
     for lambda_value in lambda_values:
         x_star_cvxpy[lambda_value] = generate_trend_using_cvxpy(y, D_matrix, lambda_value, epsilon)
@@ -39,6 +40,96 @@ def main():
                                     'y_label': 'Trend'},
                        file_name='summary.png',
                        output_dir=os.path.join(output_dir, 'l1_filtering_with_cvxpy'))
+
+    # punctul d)
+    x_star_hp = {}
+    for lambda_value in lambda_values:
+        x_star_hp[lambda_value] = generate_trend_using_hp_filter(y, D_matrix, lambda_value)
+    save_data_to_pickle_file(data={'lambda_to_trend_star_dict': x_star_hp},
+                             file_name='data.pickle',
+                             output_dir=os.path.join(output_dir, 'efficient_hp_filtering'))
+    plot_optimal_trend(lambda_to_trend_star_dict=x_star_hp,
+                       original_trend=x,
+                       plot_params={'title': 'Efficient HP filtering vs original trend',
+                                    'x_label': 'Time',
+                                    'y_label': 'Trend'},
+                       file_name='summary.png',
+                       output_dir=os.path.join(output_dir, 'efficient_hp_filtering'))
+
+
+def generate_trend_using_hp_filter(y, D_matrix, lambda_value):
+    if not (type(y) is np.ndarray and y.ndim == 1):
+        raise ValueError('parameter y should be a 1D numpy array!')
+    n = y.shape[0]
+    if not (type(D_matrix) is np.ndarray and D_matrix.shape == (n - 2, n)):
+        raise ValueError('parameter D_matrix should be a 2D numpy array with shape (n-2,n), where n=y.shape[0]!')
+
+    # trebuie sa obtinem in mod eficient pe x astfel incat Ax=b, unde A si b sunt:
+    A = np.eye(n) + 2 * lambda_value * D_matrix.transpose().dot(D_matrix)
+    b = y
+
+    # cate va observatii despre matricea A:
+    # A este o matrice definita pe multimea numerelor reale
+    assert A.dtype == 'float64'
+    # deoarece D este o matrice cu shape (n-2,n) -> A este o matrice cu shape (n,n)
+    assert A.shape == (n, n)
+    # A este simetrica -> toate valorile proprii sunt din multimea numerelor reale
+    assert np.allclose(A, A.transpose())
+    # A este o matrice definita pe multimea numerelor reale si simetrica -> A este si o matrice Hermitiana
+    # A este pozitiv definita -> toate valorile proprii sunt strict mai mari ca 0
+    A_eigenvalues = np.linalg.eigvals(A)
+    assert np.all(A_eigenvalues > 0)
+    # A este o matrice cu exact 5 diagonale indiferent pentru orice n>=5
+    #   * o diagonala principala
+    #   * doua supra diagonale
+    #   * doua sub diagonale
+    no_over_diagonals = np.sum(A[0, :] != 0) - 1
+    no_under_diagonals = np.sum(A[:, 0] != 0) - 1
+    assert no_over_diagonals == 2 and no_under_diagonals == 2
+    # matricea A mai este cunoscuta si ca o matrice pentadiagonala
+    # in scipy matricea pentadiagonala este un caz particular pentru banded matrices (matrice formata din benzi adica din mai multe diagonale)
+    # am putea folosi functia scipy.linalg.solve_banded pentru a rezolva sistemul Ax=b
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.linalg.solve_banded.html
+    # dar stim ca A este si o matrice Hermitiana pozitiv definita -> simplificam problema
+    # adica folosim functia scipy.linalg.solveh_banded
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.linalg.solveh_banded.html
+    # pentru a utiliza functia solveh_banded vom avea nevoie doar de diagonala princiapla si cele 2 sub diagonale deoarece A este Hermitiana si pozitiv definita
+    # alternativ se poate utiliza si doar diagonala principala si cele 2 supra diagonale
+    # practic A poate fi definita echivalent cu o matrice tridiagonala
+    # functia scipy.linalg.solveh_banded utilizeaza algoritmul lui Thomas care are complexitatea O(n)
+    # deci sistemul Ax=b se rezolva in timp O(n)
+
+    principal_diagonal = np.diag(A)
+    first_sub_diagonal = np.diag(A, -1)
+    second_sub_diagonal = np.diag(A, -2)
+
+    # fac padding cu 0 la subdiagonale astfel incat acestea sa aiba acelasi nr de elemente ca diagonala princiala
+    first_sub_diagonal = np.append(first_sub_diagonal, np.zeros(shape=n - first_sub_diagonal.shape[0]))
+    second_sub_diagonal = np.append(second_sub_diagonal, np.zeros(shape=n - second_sub_diagonal.shape[0]))
+
+    # construiesc matricea echivalenta cu A pentru a fi utilizata in functia scipy.linalg.solveh_banded
+    # noua matrice va contine doar diagonala principala si cele 2 sub diagonale
+    # functia scipy.linalg.solveh_banded cere ca input pentru parametrul ab doar cele 3 diagonale deoarece toate celelalte elemente sunt 0
+    ab_v1 = np.array([principal_diagonal, first_sub_diagonal, second_sub_diagonal])
+
+    # utiliam functia scipy.linalg.solveh_banded pentru a rezolva sistemul Ax=b
+    x_star_efficient_v1 = scipy.linalg.solveh_banded(ab=ab_v1, b=b, lower=True)
+
+    # echivalent se poate implementa algoritmul descris mai sus utilizand pe landa diagonala principala
+    # si doar pe cele 2 supra diagonale
+    first_supra_diagonal = np.diag(A, 1)
+    first_supra_diagonal = np.append(np.zeros(n - first_supra_diagonal.shape[0]), first_supra_diagonal)
+    second_supra_diagonal = np.diag(A, 2)
+    second_supra_diagonal = np.append(np.zeros(n - second_supra_diagonal.shape[0]), second_supra_diagonal)
+    ab_v2 = np.array([second_supra_diagonal, first_supra_diagonal, principal_diagonal])
+    x_star_efficient_v2 = scipy.linalg.solveh_banded(ab=ab_v2, b=b)
+    assert np.allclose(x_star_efficient_v1, x_star_efficient_v2)
+
+    # testez implementarea de mai sus cu implementarea ineficienta Ax=b utilizand np.linalg.solve
+    x_star_inefficient = np.linalg.solve(A, b)
+    assert np.allclose(x_star_efficient_v1, x_star_inefficient)
+
+    return x_star_efficient_v1
 
 
 def plot_optimal_trend(lambda_to_trend_star_dict, original_trend, plot_params, file_name, output_dir):
